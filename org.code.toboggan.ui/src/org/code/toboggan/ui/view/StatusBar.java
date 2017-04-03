1e984fcbc1abce4a6ba9b941bd1aaa54429ecc36
Benedict Wong <bennydictwong@gmail.com>
2017-04-03 00:02:39 -0400

    0.0.40 - Removed setting of colors for status bar
    Signed-off-by: Benedict Wong <bennydictwong@gmail.com>

--- org.code.toboggan.ui/src/org/code/toboggan/ui/view/StatusBar.java
+++ org.code.toboggan.ui/src/org/code/toboggan/ui/view/StatusBar.java
@@ -1,24 +1,16 @@
 package org.code.toboggan.ui.view;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.ui.PlatformUI;
 
 public class StatusBar extends Composite {
 	private Label label;
-	private Color color;
 
 	public StatusBar(Composite parent, int style) {
 		super(parent, style);
-		this.initialize();
-		
-		color = PlatformUI.getWorkbench().getThemeManager().getCurrentTheme().getColorRegistry().get("org.eclipse.ui.editors.backgroundColor");
-		if (color != null){
-			this.setBackground(color);
-		}		
+		this.initialize();		
 	}
 
 	private void initialize() {		
@@ -31,9 +23,6 @@ public class StatusBar extends Composite {
 		this.setLayout(layout);
 		
 		label = new Label(this, SWT.NONE);
-		if (color != null){
-			label.setBackground(color);
-		}
 	}
 
 	public void setStatus(String status) {
